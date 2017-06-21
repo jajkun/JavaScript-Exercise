8656cd892c8b415f552c63f93c095cd459272abb
jajkun <jajkunnusa@gmail.com>
2017-06-21 10:32:21 +0800

    Update exercise2.js

--- exercise2.js
+++ exercise2.js
@@ -1,4 +1,4 @@
-
+//找出元素 item 在给定数组 arr 中的位置 例: input:indexOf([ 1, 2, 3, 4 ], 3) output:2
 function indexOf(a,number){
 	for(var i in a){
 		if(number==a[i]){
@@ -6,7 +6,14 @@ function indexOf(a,number){
 		}
 	}
 }//indexOf
-
-
 indexOf([1,2,3,4],3);
-console.log();
+
+//计算给定数组 arr 中所有元素的总和 例：input: sum([ 1, 2, 3, 4 ]) output:10
+function sum(array){
+	var sum = 0;
+	for(var i in array){
+		sum+=array[i];
+	}//for
+	return sum;
+}
+console.log(sum([1,2,3]));
