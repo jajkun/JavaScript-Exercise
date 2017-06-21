2b07801816e2ba0f580594bc9f33c8ffed82f79c
jajkun <jajkunnusa@gmail.com>
2017-06-21 09:32:34 +0800

    Add files via upload

---
+++ exercise1.js
@@ -0,0 +1,33 @@
+//------- [4,3,3,6,3,1,6] to print 3 out? --------------
+function print(array){
+	//first to filter array
+	var filterArray=[];
+	var countAppears;
+	var printArray=[];
+	for( var i in array ){
+		if( filterArray.indexOf(array[i])==-1 ){
+			filterArray.push(array[i]);
+		}//if
+	}
+	//calculate each numbers count
+	for( var j in filterArray ){
+		countAppears = calcCount(array,filterArray[j]);
+		if( countAppears>1 ){
+			printArray.push(filterArray[j]);
+		}//if
+	}//for
+	console.log(printArray);
+}//print
+
+function calcCount(original,filters){
+	var count=0;
+	original.map(function(a){
+		if( a==filters ){
+			count++;
+		}//if
+	});
+	return count;
+}//calcCount
+
+var a=[4,3,3,6,3,1,6];
+print(a); //3
