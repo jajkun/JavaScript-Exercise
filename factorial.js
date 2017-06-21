ff96dd299104d12d288f45ba58ac35929b5489d1
jajkun <jajkunnusa@gmail.com>
2017-06-21 09:47:54 +0800

    Add files via upload

---
+++ factorial.js
@@ -0,0 +1,25 @@
+//recursive
+function factorialWithRecursive(number){
+	if( number==1 ){
+		return 1;
+	}
+	return number*factorialWithRecursive(number-1);
+}
+
+//loop
+function factorialWithLoop(number){
+	var result=1;
+	if( number==0 || number==1 ){
+		return 1;
+	}else{
+		for( var i=1;i<number;i++ ){
+			result *= i; //result * i = result;
+		}//for
+		return result;
+	}//else
+	
+}
+
+var result = factorialWithRecursive(10);
+//var result = factorialWithLoop(1);
+console.log(result);
